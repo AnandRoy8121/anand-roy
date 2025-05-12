7b5c7a1634496b3e6d03a76eb2d71e3059df4f9f
anandr@gerabsys.com <anandroy151997@gmail.com>
2025-05-12 12:04:04 +0530

    added github actions and logic

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: '/anand-roy', // Base path for the application
+  output: 'export', // Enable static export
   reactStrictMode: true,
 }
 const withTM = require('next-transpile-modules')(['three'])
